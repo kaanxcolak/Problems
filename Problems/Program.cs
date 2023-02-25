fc2ceb2fb45a09dee8a99b6cd562168cefe87739
Kaan Colak <kaancolak3458@gmail.com>
2023-02-25 22:53:09 +0300

    Add project files.

---
+++ Problems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ VeriGirisi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VeriGirisi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
