464e8c3554536f374e15ff73411f12a2f7471490
Kaan Colak <kaancolak3458@gmail.com>
2023-02-25 23:03:30 +0300

    updated

--- VeriGirisi/Program.cs
+++ VeriGirisi/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string ad;
+            ConsoleKeyInfo h;
+            Console.Clear(); Console.WriteLine("İsim: ");
+            ad = Console.ReadLine(); Console.WriteLine($"\nMerhaba {ad}");
+            do
+            {
+                Console.WriteLine("\nÇıkış için X tuşu...");
+                h = Console.ReadKey();
+            } while (h.KeyChar != 'x');
         }
     }
 }
