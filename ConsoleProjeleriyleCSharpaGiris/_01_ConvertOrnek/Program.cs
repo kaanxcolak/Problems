4a3d4074be71eaaef8d5a581979e5a4697c7bdec
Kaan Colak <kaancolak3458@gmail.com>
2023-03-22 23:18:58 +0300

    _o1_ created and updated

---
+++ ConsoleProjeleriyleCSharpaGiris/_01_ConvertOrnek/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01_ConvertOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Amaç: int tipli değişkeni kullanmak
+            //Convert ile tanışmak
+            //Özetle Convert dönüştürmek demektir. Console.ReadLine string bir tiptedir. Oysa bize int tip gerekiyor
+            //Bu nedenle dönüşüm yaptık. Console uygulamasında convert sınıfını çokça kullanacağım!!!
+            //Kullanıcıdan 2 sayı alıp toplayalım
+            int sayi1 = 0, sayi2 = 0;
+
+            Console.WriteLine("Bir sayi giriniz{sayi1 + sayi2}: ");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bir sayı daha giriniz: ");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Sonuc = {sayi1 + sayi2}");
+            Console.ReadKey();
+        }
+        
+    }
+}
