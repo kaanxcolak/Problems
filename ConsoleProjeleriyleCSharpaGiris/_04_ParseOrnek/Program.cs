a70b2d21cc605179fab92994676cf52e36ed5626
Kaan Colak <kaancolak3458@gmail.com>
2023-03-26 23:06:24 +0300

    created

---
+++ ConsoleProjeleriyleCSharpaGiris/_04_ParseOrnek/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _04_ParseOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SORU: Convert haricinde hangi galaksiler var ??
+            //Parse diye bir eleman daha varmış. Haydi tanışalım.
+            //Parse'ı google dan hemen araştırdığınızda onun da tıpkı Convert gibi dönüştürme işlemi yaptığını görececksiniz.
+            //Convert bir classtır (oopta class detaylı işlenecek)
+            //Parse bir metottur.
+
+            Console.WriteLine("Bir sayı giriniz    : ");
+            //haydi deneyelim 20 yazın patlamayacak 20, yazın ya da rakam harici yazın patlayacak....
+            int sayi = int.Parse(Console.ReadLine());
+            Console.WriteLine("Teşekkürler");
+
+            Console.ReadKey();
+        }
+    }
+}
