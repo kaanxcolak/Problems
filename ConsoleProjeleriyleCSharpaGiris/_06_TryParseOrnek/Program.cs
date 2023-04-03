8387dda71335d9ca4c910e1f62c3ffd3c9b178aa
Kaan Colak <kaancolak3458@gmail.com>
2023-04-03 21:58:20 +0300

    created

---
+++ ConsoleProjeleriyleCSharpaGiris/_06_TryParseOrnek/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _06_TryParseOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Yazılımcılar olarak her zaman temkinli olmalıyız
+            //İyi yazılımcı hızlı kod yazandan öte detayları düşünüp en az patlama ile temiz kod yazan yazılımcıdır
+            //Try Parse için Parse'ın kuzeni diyebiliriz.
+            
+            Console.WriteLine("****************\n"); // /n ile alt satıra geçme işlemi yapılır.
+            int sayi = 0;
+            Console.WriteLine("Bir sayi giriniz: ");
+
+            bool kontrol = Int32.TryParse(Console.ReadLine(), out sayi);
+            Console.WriteLine($"Girdiğiniz değer integer'a uygun mudur? Cevap: {kontrol}");
+        }
+    }
+}
