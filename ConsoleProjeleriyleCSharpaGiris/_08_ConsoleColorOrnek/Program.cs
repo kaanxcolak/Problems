fccf881de9f8dbf111567935bbdd06eda7cb1844
Kaan Colak <kaancolak3458@gmail.com>
2023-04-04 18:04:58 +0300

    create new example

---
+++ ConsoleProjeleriyleCSharpaGiris/_08_ConsoleColorOrnek/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _08_ConsoleColorOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //C# tip bağımlı bir dil demiştik.
+            //Burada onu bir kez daha inceleyelim
+            //Tipler sadece öğrendiğinniz int decimal bool string char datetime mıdır?
+            //HAYIR
+            //C#'ta enum yapıları tip olabilir interfaceler ve classlar tip olabilir 
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Renk kırmızı oldu!");
+            // Console.ResetColor(); // renkleri default ayarına getirir
+            Console.ReadKey();
+        }
+    }
+}
