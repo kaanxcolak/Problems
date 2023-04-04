efd8d0e5778b0472aada31b60bb3f1b7675b7296
Kaan Colak <kaancolak3458@gmail.com>
2023-04-04 18:01:02 +0300

    created new example

---
+++ ConsoleProjeleriyleCSharpaGiris/_07_TryParseOrnek/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _07_TryParseOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ornek: Dolar kuru
+
+            double miktarUSD = 0;
+            double dolarKuru = 0;
+            double sonuc = 0;
+
+            Console.WriteLine("Dolar kuru giriniz: ");
+            double.TryParse(Console.ReadLine(), out dolarKuru);
+            // double.Tryparse yani double dönüştürmeya uğraş
+            // yapabilirse dönüştürdğünü out ile keywordü aracılığıyla dışarıdaki değişkene aktar.
+            Console.WriteLine("Kaç dolarınız var? :   ");
+            double.TryParse(Console.ReadLine(), out miktarUSD);
+            sonuc = miktarUSD * dolarKuru;
+            Console.WriteLine($"{dolarKuru} * {miktarUSD} = {sonuc} TL paranız var.");
+            // String ifadeleri 3 şekilde yazabiliriz
+            // 1--> + operatörleri ile stringleri birleştirebiliriz
+            // 2 --> string format ile yazabilirsiniz.
+            // 3--> string formatın $ işareti haliyle.
+            Console.ReadKey();
+        }
+    }
+}
