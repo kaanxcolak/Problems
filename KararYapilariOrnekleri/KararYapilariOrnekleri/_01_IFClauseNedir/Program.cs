cd6ca664a5ba3228219d06a0dd6507b0ba32c897
kaanc <kaancolak3458@gmail.com>
2023-05-12 11:47:30 +0300

    updated

--- KararYapilariOrnekleri/KararYapilariOrnekleri/_01_IFClauseNedir/Program.cs
+++ KararYapilariOrnekleri/KararYapilariOrnekleri/_01_IFClauseNedir/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_IFClauseNedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // Karar yapısı ne demektir?        
+        // if bloğu içinde true ya da false değer dönürerek true geldiyse kod bloğu içine girer ya da false gelirse girmez.
+
+        
+
+        Baslangic:
+            int sayi1 = 0;
+            Console.Write("Sayı Gir     :");
+            bool kontrol1 = Int32.TryParse(Console.ReadLine(), out sayi1);
+
+
+
+            //Eğer kullanıcı gerçekten sayısal değer girdiyse tryparse bana evet bu sayısal diyecek yani true verecektir.
+            //if (kontrol1 == true) ya da if(kontrol1) kontrol1 zaten boolean olduğu için tekrar eşitlemeden kullanabiliirsiniz
+            if (kontrol1)
+            {
+                Console.WriteLine("Doğru giriş yaptınız!");
+            }
+            //if (kontrol1 != true)
+            else
+            {
+                Console.WriteLine("YANLIŞ giriş yaptınız! Sizi başa yönlendiriyoruz...");
+                Console.WriteLine("\n");
+                goto Baslangic;
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
